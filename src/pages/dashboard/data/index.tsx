5485f93cd8b5821204f4fbcffd81a9a9c6d7b612
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-11-03 11:23:48 -0300

    feat: data component

--- src/pages/dashboard/data/index.tsx
+++ src/pages/dashboard/data/index.tsx
@@ -1,3 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function Data() {
-  return <h1>Data</h1>;
+  const { id } = useParams<string>();
+  const [pokemon, setPokemon] = useState<any>(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(({ data }) => {
+        setPokemon(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  return (
+    <div className="text-slate-200 flex flex-col px-2">
+      <h1> Weight: {pokemon?.weight} </h1>
+      <h1> Height: {pokemon?.height} </h1>
+      <h1> Experience: {pokemon?.base_experience} </h1>
+    </div>
+  );
 }
