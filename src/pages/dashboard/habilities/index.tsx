56c44012e7313444ffcf6181bb5fc51c164631be
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-11-03 11:27:47 -0300

    feat: component habilities

--- src/pages/dashboard/habilities/index.tsx
+++ src/pages/dashboard/habilities/index.tsx
@@ -1,3 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function Habilities() {
-  return <h1>Habilities</h1>;
+  const { id } = useParams<string>();
+  const [pokemon, setPokemon] = useState<any>(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(({ data }) => {
+        setPokemon(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  function format(str: string) {
+    return (
+      <span key={str} className="text-lg capitalize">
+        {str.replace("-", " ")}
+      </span>
+    );
+  }
+
+  return (
+    <div className="text-slate-200 flex flex-col gap-2 w-full px-2">
+      {pokemon?.abilities.map((hab: any) => format(hab?.ability?.name))}
+    </div>
+  );
 }
