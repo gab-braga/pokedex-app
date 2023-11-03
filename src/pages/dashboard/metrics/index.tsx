868b1b3bd6c1f4efe6c267f091c445d9e330ede8
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-11-03 11:32:25 -0300

    feat: metrics component

--- src/pages/dashboard/metrics/index.tsx
+++ src/pages/dashboard/metrics/index.tsx
@@ -1,3 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function Metrics() {
-  return <h1>Metrics</h1>;
+  const { id } = useParams<string>();
+  const [pokemon, setPokemon] = useState<any>(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(({ data }) => {
+        setPokemon(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  function format(str: string) {
+    return (
+      <span key={str} className="text-lg capitalize">
+        {str.replaceAll("-", " ")}
+      </span>
+    );
+  }
+
+  return (
+    <div className="text-slate-200 flex flex-col gap-2 w-full px-2">
+      {pokemon?.stats.map((stat: any) => (
+        <div key={stat?.stat?.name}>
+          {format(stat?.stat?.name)}: {stat?.base_stat}
+        </div>
+      ))}
+    </div>
+  );
 }
